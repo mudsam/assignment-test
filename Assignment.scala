c2cb3a975019663e6466b1ac2e5446480a9e36ca
mudsam <johan@mudsam.com>
2018-01-14 12:31:53 -0600

    Update Assignment.scala

--- Assignment.scala
+++ Assignment.scala
@@ -1,7 +1,42 @@
+import scala.util.{Failure, Success, Try}
+
 object Assignment {
-/*** No changes above this line ***/
+
+  /**
+    * Calculate the n-th number in the fibonacci sequence
+    * based on the command line input.
+    *
+    * Will print the output to the console.
+    *
+    * @param args The command line arguments
+    */
   def main(args: Seq[String]): Unit = {
-    println(args)
+    require(args.length != 1, "One input argument needed")
+
+    Try(args(0).trim.toInt) match {
+      case Success(n) => println(fibonacci(args(0).toInt))
+      case Failure(e) => println(s"Failed to parse input - $e")
+    }
+
   }
-/*** No changes below this line ***/
+
+  /*** No changes above this line ***/
+
+  /**
+    * Method to calculate the n-th number in the fibonacci sequence
+    *
+    * @param n which number in the fibonacci sequence to calculate, where n > 0
+    * @return the n-th number in the fibonacci sequence
+    */
+  def fibonacci(n: Int): Int = {
+    def fibs(n:Int):(Int, Int) = if (n == 1) (1,0) else {
+      val (a,b) = fibs(n/2)
+      val p = (2*b+a)*a
+      val q = a*a + b*b
+      if(n % 2 == 0) (p,q) else (p+q,p)
+    }
+    fibs(n)._1
+  }
+
+  /*** No changes below this line ***/
 }
